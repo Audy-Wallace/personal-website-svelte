e1a12857daf5d7dd08307fbe30717cd18c846c55
Audy Wallace Siegle <hwksin7@gmail.com>
2022-06-29 22:21:18 +0700

    add background color and create horizontal navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<script>
+    function linkHandler(event){};
+</script>
+<nav class="p-6">
+    <ul class="flex items-center justify-center gap-96">
+        <li><a class="hover:underline" href="/" on:click|preventDefault={linkHandler}>About</a></li>
+        <li><a class="hover:underline" href="/" on:click|preventDefault={linkHandler}>Contact</a></li>
+    </ul>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,12 @@
-
+<div class="index-parent min-h-fit h-screen min-w-fit w-screen">
+    <Navbar/>
+</div>
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+<style>
+.index-parent{
+    background-color: #29539b;
+    background-image: linear-gradient(315deg, #29539b 0%, #1e3b70 74%);
+}
+</style>
