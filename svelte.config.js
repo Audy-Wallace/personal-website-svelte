65c899c5fa5196a379afb1e790d28dcbe6c04ef5
Audy Wallace Siegle <hwksin7@gmail.com>
2022-06-29 21:50:19 +0700

    import css to svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte/types/compiler/preprocess';
+import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
