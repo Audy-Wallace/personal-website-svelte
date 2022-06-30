610b69e52a8ece770f52a0eccccd61be1a1fa339
Audy Wallace Siegle <hwksin7@gmail.com>
2022-06-30 23:13:37 +0700

    add logo

---
+++ src/components/Navbar/HorizontalNav.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Logo from './Logo.svelte';
+	function linkHandler(event) {}
+</script>
+
+<div class="flex justify-between">
+	<Logo />
+	<ul class="flex items-center">
+		<li>
+			<a
+				class="text-[#fefefe] text-2xl hover:underline font-semibold"
+				href="/"
+				on:click|preventDefault={linkHandler}>About</a
+			>
+		</li>
+		<li>
+			<a
+				class="text-[#fefefe] text-2xl hover:underline font-semibold"
+				href="/"
+				on:click|preventDefault={linkHandler}>Contact</a
+			>
+		</li>
+	</ul>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,14 @@
-<div class="index-parent min-h-fit h-screen min-w-fit w-screen">
-    <Navbar/>
-</div>
 <script>
-    import Navbar from "../components/Navbar.svelte";
+	import Navbar from '../components/Navbar/Navbar.svelte';
 </script>
+
+<div class="index-parent min-h-fit h-screen min-w-fit w-screen">
+	<Navbar />
+</div>
+
 <style>
-.index-parent{
-    background-color: #29539b;
-    background-image: linear-gradient(315deg, #29539b 0%, #1e3b70 74%);
-}
+	.index-parent {
+		background-color: #29539b;
+		background-image: linear-gradient(315deg, #29539b 0%, #1e3b70 74%);
+	}
 </style>
